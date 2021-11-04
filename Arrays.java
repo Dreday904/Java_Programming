88bb80c1e0ea454e5fcf9280e54b4143e675e9ec
Dreday <54422312+Dreday904@users.noreply.github.com>
2021-11-04 08:24:19 -0400

    Create Arrays.java

---
+++ Arrays.java
@@ -0,0 +1,23 @@
+package tutorial;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[]newArr = new String[5];
+		newArr[0] = "hello"; 
+		newArr[1] = "hi"; 
+		newArr[2] = "dre"; 
+		newArr[3] = "joe"; 
+		newArr[4] = "tim"; 
+		
+		int[] num = {2,3,54,6,6};
+		
+		double[] num2 = {2.0, 3.0};
+		
+		int x = num[4];
+		System.out.println(x);
+	}
+
+}
